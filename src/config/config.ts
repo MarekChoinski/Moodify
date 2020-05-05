09b405e4b3337e6ecb5f5384158994f881440f8b
MarekChoinski <marekchoinski17@gmail.com>
2020-05-05 20:02:39 +0200

    Config is now public

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+export default {
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+    clientId: "056a2f6da4284c55980882095f831b4d",
+    redirectUri: "https://clever-sammet-231c42.netlify.app/",
+    // redirectUri: "http://localhost:3000/",
+    scopes: [
+        "user-library-read",
+        "user-modify-playback-state",
+    ],
+};
+
+export const maxSongs: number = 10000;
