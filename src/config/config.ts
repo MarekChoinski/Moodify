91791491d4f1d12fdd3bea87bd3563de074d7d05
MarekChoinski <marekchoinski17@gmail.com>
2020-05-06 12:29:04 +0200

    Changing to localhost

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 export default {
     authEndpoint: 'https://accounts.spotify.com/authorize',
     clientId: "056a2f6da4284c55980882095f831b4d",
-    redirectUri: "https://clever-sammet-231c42.netlify.app/",
-    // redirectUri: "http://localhost:3000/",
+    // redirectUri: "https://clever-sammet-231c42.netlify.app/",
+    redirectUri: "http://localhost:3000/",
     scopes: [
         "user-library-read",
         "user-modify-playback-state",
