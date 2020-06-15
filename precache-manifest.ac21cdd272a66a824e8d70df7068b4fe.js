856be240ccb1432ea0b457e875703d2323fa55a9
Marek Choiński <mchoinski@in.eobuwie.pl>
2020-06-15 22:55:39 +0200

    Updates

---
+++ precache-manifest.ac21cdd272a66a824e8d70df7068b4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5eb07945e254faebe8edb026357799b",
+    "url": "/Moodify/index.html"
+  },
+  {
+    "revision": "a17e00834ab3f983424d",
+    "url": "/Moodify/static/css/main.f42a44c2.chunk.css"
+  },
+  {
+    "revision": "3b7369b1efaa3acfba61",
+    "url": "/Moodify/static/js/2.28c86173.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/Moodify/static/js/2.28c86173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17e00834ab3f983424d",
+    "url": "/Moodify/static/js/main.ba4903b7.chunk.js"
+  },
+  {
+    "revision": "689d635324a3c1203492",
+    "url": "/Moodify/static/js/runtime-main.1f43dcab.js"
+  }
+]);
