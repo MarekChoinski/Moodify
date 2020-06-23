b9b4d19498bb20113a05525ea37a01ec89a56369
Marek Choiński <marekchoinski17@gmail.com>
2020-06-23 13:27:13 +0200

    Edit homepage

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,7 @@
 export default {
     authEndpoint: 'https://accounts.spotify.com/authorize',
     clientId: "056a2f6da4284c55980882095f831b4d",
-    //redirectUri: process.env.NODE_ENV !== "production" ? "http://localhost:3000/": "https://moodify.marekchoinski.com/",
-    redirectUri: "https://moodify.marekchoinski.com/",
+    redirectUri: process.env.NODE_ENV !== "production" ? "http://localhost:3000/": "https://moodify.marekchoinski.com/",
     scopes: [
         "user-library-read",
         "user-modify-playback-state",
